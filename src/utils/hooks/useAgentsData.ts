2033ad763207ad1c7b4d997072d82f8c0af448bf
Vonagre213-RD <silveriodenzel@gmail.com>
2025-08-17 17:52:06 -0400

    Fixed agents url

--- src/utils/hooks/useAgentsData.ts
+++ src/utils/hooks/useAgentsData.ts
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import type { AgentDataType } from "@/Types/agents";
 
-const baseUrl = "https://zenlesszonezeroapi.onrender.com/characters/agents/";
 export default function useAgentsData() {
   const [agentsData, setAgentsData] = useState<AgentDataType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -13,7 +12,7 @@ export default function useAgentsData() {
       try {
         setIsLoading(true);
 
-        const res = await fetch("https://zenless-zone-zero-api-private.onrender.com/All");
+        const res = await fetch("https://zenless-zone-zero-api-private.onrender.com/characters/agents/all");
         
         const data: AgentDataType[] = await res.json();
 
